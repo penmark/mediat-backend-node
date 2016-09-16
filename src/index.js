a50d5ccc3906c3c68146815f34e2068ab1dfaa39
Pontus Enmark <pontus@wka.se>
2016-09-16 21:09:38 +0200

    Reformat

--- src/index.js
+++ src/index.js
@@ -9,25 +9,25 @@ const util = require('./util')
 const app = express();
 
 app.use(morgan('dev'))
-    .use(bodyParser.urlencoded({extended: true}))
-    .use(bodyParser.json())
-    .use(util.jsonQuery())
-    .use(util.errorHandler())
+  .use(bodyParser.urlencoded({extended: true}))
+  .use(bodyParser.json())
+  .use(util.jsonQuery())
+  .use(util.errorHandler())
 
 app.use('/item', item)
 
 db.connect('mongodb://localhost:27017/media', (err) => {
+  if (err) {
+    console.log('Unable to connect to Mongo.')
+    process.exit(1)
+  }
+  db.createIndexes('item', {title: 1, modified: -1}, (err) => {
     if (err) {
-        console.log('Unable to connect to Mongo.')
-        process.exit(1)
+      console.log('Unable to create indexes', err)
+      process.exit(2)
     }
-    db.createIndexes('item', {title: 1, modified: -1}, (err) => {
-        if (err) {
-            console.log('Unable to create indexes', err)
-            process.exit(2)
-        }
-    })
-    app.listen(3000, () => {
-        console.log('Listening on port 3000...')
-    })
+  })
+  app.listen(3000, () => {
+    console.log('Listening on port 3000...')
+  })
 })
--- src/item.js
+++ src/item.js
@@ -6,44 +6,44 @@ const util = require('./util')
 
 
 function* find(req, res, next) {
-    const items = yield db.collection('item').find(req.jsonQuery.get('query'))
-        .project(req.jsonQuery.get('projection'))
-        .sort(req.jsonQuery.get('sort'))
-        .limit(req.jsonQuery.get('limit') || 0)
-        .skip(req.jsonQuery.get('skip') || 0)
-        .toArray()
-    res.send(items)
+  const items = yield db.collection('item').find(req.jsonQuery.get('query'))
+    .project(req.jsonQuery.get('projection'))
+    .sort(req.jsonQuery.get('sort'))
+    .limit(req.jsonQuery.get('limit') || 0)
+    .skip(req.jsonQuery.get('skip') || 0)
+    .toArray()
+  res.send(items)
 }
 
 function* findOne(req, res, next) {
-    const item = yield db.collection('item')
-        .findOne({_id: req._id}, req.jsonQuery.projection)
-        .then(item => {
-            if (!item) {
-                let err = new Error('Not found')
-                err.status = 404
-                return next(err)
-            }
-            res.send(item)
-        })
+  const item = yield db.collection('item')
+    .findOne({_id: req._id}, req.jsonQuery.projection)
+    .then(item => {
+      if (!item) {
+        let err = new Error('Not found')
+        err.status = 404
+        return next(err)
+      }
+      res.send(item)
+    })
 }
 
 function* updateOne(req, res, next) {
-    const item = yield db.collection('item')
-        .updateOne({_id: req._id}, req.jsonQuery.update)
-    res.send({ok: result.ok})
+  const item = yield db.collection('item')
+    .updateOne({_id: req._id}, req.jsonQuery.update)
+  res.send({ok: result.ok})
 }
 
 const router = Router()
 
 router.param('_id', (req, res, next, _id) => {
-    try {
-        req._id = ObjectId.createFromHexString(_id)
-    } catch (err) {
-        err.status = 400
-        next(err)
-    }
-    next();
+  try {
+    req._id = ObjectId.createFromHexString(_id)
+  } catch (err) {
+    err.status = 400
+    next(err)
+  }
+  next();
 })
 
 router.get('/', util.wrap(find))
